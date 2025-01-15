ce7a4d35f8571f7fa15b0522fbf9f2ca889875e7
Aiden Shymanski <shymanski.a@northeastern.edu>
2025-01-15 16:28:41 -0500

    Final Draft

--- src/Kambaz/Courses/Assignments/Editor.tsx
+++ src/Kambaz/Courses/Assignments/Editor.tsx
@@ -1,23 +1,104 @@
 export default function AssignmentEditor() {
-    return (
-      <div id="wd-assignments-editor">
-        <label htmlFor="wd-name">Assignment Name</label>
-        <input id="wd-name" value="A1 - ENV + HTML" /><br /><br />
-        <textarea id="wd-description">
-          The assignment is available online Submit a link to the landing page of
-        </textarea>
-        <br />
-        <table>
-          <tr>
-            <td align="right" valign="top">
-              <label htmlFor="wd-points">Points</label>
-            </td>
-            <td>
-              <input id="wd-points" value={100} />
-            </td>
-          </tr>
-          {/* Complete on your own */}
-        </table>
-      </div>
-  );}
-  
+  return (
+    <div id="wd-assignments-editor">
+      <label htmlFor="wd-name"><strong>Assignment Name</strong></label><br /><br />
+      <input id="wd-name" value="A1 - ENV + HTML" /><br /><br />
+      <textarea id="wd-description">
+        The assignment is available online Submit a link to the landing page of your Web application running on Netlify. The landing page should include the following: Your full name and section Links to each of the lab assignments Link to the Kanbaz application Links to all relevant source code repositories The Kanbaz application should include a link to navigate back to the landing page.
+      </textarea>
+      <br /> <br />
+      <table>
+        <tr>
+          <td align="right" valign="top">
+            <label htmlFor="wd-points">Points</label>
+          </td>
+          <td>
+            <input id="wd-points" value={100} />
+          </td>
+        </tr> <br />
+        <tr>
+          <td align="right" valign="top">
+            <label htmlFor="wd-group">Assignment Group</label>
+          </td>
+          <td>
+            <select id="wd-group">
+              <option selected value="ASSIGNMENTS">
+                ASSIGNMENTS</option>
+              <option value="QUIZZES">QUIZZES</option>
+              <option value="EXAMS">EXAM</option>
+              <option value="PROJECT">PROJECT</option>
+            </select>
+          </td>
+        </tr> <br />
+        <tr>
+          <td align="right" valign="top">
+            <label htmlFor="wd-display-grade-as">Display Grade as</label>
+          </td>
+          <td>
+            <select id="wd-display-grade-as">
+              <option selected value="PERCENTAGE">
+                Percentage</option>
+              <option value="LETTER">Letter</option>
+            </select>
+          </td>
+        </tr> <br />
+        <tr>
+          <td align="right" valign="top">
+            <label htmlFor="wd-submission-type">Submission Type</label>
+          </td>
+          <td>
+            <select id="wd-submission-type">
+              <option selected value="ONLINE">
+                Online</option>
+              <option value="INPERSON">In-Person</option>
+            </select><br /><br />
+            <label>Online Entry Options</label><br />
+
+            <input type="checkbox" name="check-entry" id="wd-text-entry" />
+            <label htmlFor="wd-text-entry">Text Entry</label><br />
+
+            <input type="checkbox" name="check-entry" id="wd-website-url" />
+            <label htmlFor="wd-website-url">Website URL</label><br />
+
+            <input type="checkbox" name="check-entry" id="wd-media-recordings" />
+            <label htmlFor="wd-media-recordings">Media Recordings</label><br />
+
+            <input type="checkbox" name="check-entry" id="wd-student-annotation" />
+            <label htmlFor="wd-student-annotation">Student Annotation</label><br />
+
+            <input type="checkbox" name="check-entry" id="wd-file-upload" />
+            <label htmlFor="wd-file-upload">File Uploads</label>
+          </td>
+        </tr> <br />
+        <tr>
+          <td align="right" valign="top">
+            <label>Assign</label>
+          </td>
+          <td>
+            <label htmlFor="wd-assign-to">Assign to</label><br />
+            <input id="wd-assign-to" value="Everyone" /> <br /> <br />
+            <label htmlFor="wd-due-date">Due</label><br />
+            <input type="date"
+              value="2024-05-13"
+              id="wd-due-date" /> <br /> <br />
+            <table>
+              <tr>
+                <td align="left"><label htmlFor="wd-available-from">Assign from</label></td>
+                <td><label htmlFor="wd-available-until">Until</label></td>
+              </tr>
+              <tr>
+                <td><input type="date" value="2024-05-06" id="wd-available-from" /></td>
+                <td><input type="date" value="2024-05-20" id="wd-available-until" /></td>
+              </tr>
+            </table>
+          </td>
+        </tr>
+
+      </table>
+      <div id="wd-button">
+        <button>Cancel</button> <button>Save</button>
+        </div>
+    </div>
+    
+  );
+}
--- src/Kambaz/Courses/Home/Status.tsx
+++ src/Kambaz/Courses/Home/Status.tsx
@@ -2,7 +2,31 @@ export default function CourseStatus() {
     return (
       <div id="wd-course-status">
         <h2>Course Status</h2>
+        <div id="wd-button">
         <button>Unpublish</button> <button>Publish</button>
-        {/* Complete on your own */}
+        </div>
+        <div id="wd-blank">
+          <p></p>
+        </div>
+        <div id="wd-button">
+        <button>Import Existing Content</button>
+        </div>
+        <div id="wd-button">
+        <button>Import from Commons</button>
+        </div>
+        <div id="wd-button">
+        <button>Choose Home Page</button>
+        </div>
+        <div id="wd-button">
+        <button>View Course Stream</button>
+        </div>
+        <div id="wd-button">
+        <button>New Announcement</button>
+        </div>
+        <div id="wd-button">
+        <button>New Analytics</button>
+        </div>
+        <div id="wd-button">
         <button>View Course Notifications</button>
+        </div>
       </div> );}
